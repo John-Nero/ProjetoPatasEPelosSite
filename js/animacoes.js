0cb73e0d257ed44ce81dcb2275199af905eb3fd8
John <lennon.pbarros@gmail.com>
2024-03-08 19:34:15 -0300

    carrosel nas categorias

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,6 +1,15 @@
-$('.banner').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+/*BANNER*/
+$(".banner").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+});
+
+/*CATEGORIAS*/
+$(".carrosel_categorias").slick({
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 5000
+});
