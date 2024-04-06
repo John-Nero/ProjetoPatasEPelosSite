95ce378f5b47a12c4204b9d03586c4ef29c5c9c1
John <lennon.pbarros@gmail.com>
2024-04-06 20:04:39 -0300

    correção de tamanho part 1(menu superior, banner e agende seu horario)

--- conteudos/agenda_horario.php
+++ conteudos/agenda_horario.php
@@ -2,9 +2,11 @@
         <div class="agenda">
           <div>
             <h2 class="titulo_roxo">Agende seu horário!</h2>
-            <p>O seu pet merece o melhor tratamento, venha conferir os nossos tratamentos.</p>
+            <div><img src="img/enfeites/produtos_agenda_seu_horario.svg" alt="">
+            <p>O seu pet merece o melhor tratamento, venha conferir os nossos tratamentos.</p></div>
+            
             <a href="#">clique aqui!</a>
           </div>
-          <img src="img/mascaras/mascara_visita.svg" alt="" />
+          <img src="img/mascaras/agenda_seu_horario.svg" alt="" />
         </div>
       </section>
