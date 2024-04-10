e7f0d48865ced8be357c920463b745aba33376d6
John-Nero <lennon.pbarros@gmail.com>
2024-04-10 08:47:28 -0300

    linkagem dos menus home e serviços

--- conteudos/menu_superior.php
+++ conteudos/menu_superior.php
@@ -7,11 +7,11 @@
 
               <nav>
                 <ul class="menu_botoes">
-                  <li><a href="#">Home</a></li>
+                  <li><a href="index.php">Home</a></li>
                   <li><a href="#">Sobre Nós</a></li>
                   <li><a href="#">Produtos</a></li>
                   <li><a href="#">Contato</a></li>
-                  <li><a href="#">Serviços</a></li>
+                  <li><a href="servicos.php">Serviços</a></li>
                 </ul>
               </nav>
 
