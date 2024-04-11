7f2d4ed9e401f36a2e8838676c91c3ce25a76a00
John <lennon.pbarros@gmail.com>
2024-04-11 17:49:36 -0300

    banho e tosa menor pra caber na tela

--- conteudos/agenda_horario.php
+++ conteudos/agenda_horario.php
@@ -1,12 +1,13 @@
 <section class="site">
-  <div class="agenda">
-    <div>
-      <h2 class="titulo_roxo">Agende seu horário!</h2>
-      <div><img src="img/enfeites/produtos_agenda_seu_horario.svg" alt="">
-        <p>O seu pet merece o melhor tratamento, venha conferir os nossos tratamentos.</p>
-      </div>
-      <a href="#">clique aqui!</a>
+    <div class="agenda">
+        <div>
+            <h2 class="titulo_roxo">Agende seu horário!</h2>
+            <div>
+                <img src="img/enfeites/produtos.svg" alt="">
+                <p>O seu pet merece o melhor tratamento, venha conferir os nossos tratamentos.</p>
+            </div>
+            <a href="#">clique aqui!</a>
+        </div>
+        <img src="img/mascaras/agenda_seu_horario.svg" alt="" />
     </div>
-    <img src="img/mascaras/agenda_seu_horario.svg" alt="" />
-  </div>
 </section>
--- conteudos/banho_e_tosa.php
+++ conteudos/banho_e_tosa.php
@@ -1,13 +1,16 @@
 <section class="site">
-        <div class="tosa">
-          <img src="img/mascaras/banho_e_tosa.svg" alt="" />
-          <img src="img/mascaras/mascara_banho_mobile.svg" alt="" />
-          <div>
+    <div class="tosa">
+        <img src="img/mascaras/banho_e_tosa.svg" alt="" />
+        <img src="img/mascaras/mascara_banho_mobile.svg" alt="" />
+        <div>
             <h2 class="titulo_roxo">Fazemos:</h2>
             <h2 class="subtitulo_laranja">Tosa e Banho!</h2>
-            <p>Seu pet merece estar limpo, confira agora os nossos planos:</p>
+            <div>
+                <p>Seu pet merece estar limpo e confiante, confira agora os nossos planos:</p>
+                <img src="img\enfeites\produtos.svg" alt="">
+            </div>
 
             <a href="#">clique aqui!</a>
-          </div>
         </div>
-      </section>
+    </div>
+</section>
