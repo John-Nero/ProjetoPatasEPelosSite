1c07d5f8c8aeb9885768722b6a7f64c180941381
John-Nero <lennon.pbarros@gmail.com>
2024-05-16 11:22:56 -0300

    menu superior responsivo 900

--- conteudos/menu_superior.php
+++ conteudos/menu_superior.php
@@ -1,40 +1,40 @@
 <div class="faixa">
-  <div class="site">
-    <div class="menu_superior">
-      <div>
-        <!--H1 que vira img por config no css-->
-<button class="abrir_menu"></button>
-        
-        <h1>Patas e pelos</h1>
+    <div class="site">
+        <div class="menu_superior">
+            <div>
+                <!--H1 que vira img por config no css-->
+                <button class="abrir_menu"></button>
 
-        <nav>
-          <ul class="menu_botoes">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="#">Sobre Nós</a></li>
-            <li><a href="#">Produtos</a></li>
-            <li><a href="#">Contato</a></li>
-            <li><a href="servicos.php">Serviços</a></li>
-          </ul>
-        </nav>
+                <h1>Patas e pelos</h1>
 
-        <ul class="menu_icones">
-          <li>
-            <a href="#"><img src="img/menu/login.svg" alt="Login" /></a>
-          </li>
-          <li>
-            <a href="#"><img src="img/menu/favoritos.svg" alt="Favoritos" /></a>
-          </li>
+                <nav>
+                    <ul class="menu_botoes">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Sobre Nós</a></li>
+                        <li><a href="#">Produtos</a></li>
+                        <li><a href="#">Contato</a></li>
+                        <li><a href="servicos.php">Serviços</a></li>
+                    </ul>
+                </nav>
 
-        </ul>
+                <ul class="menu_icones">
+                    <li>
+                        <a href="#"><img src="img/menu/login.svg" alt="Login" /></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="img/menu/favoritos.svg" alt="Favoritos" /></a>
+                    </li>
 
-        <!--O Telefone passa por modificação no css para vira uma imagem-->
-        <a href="">
-          <h2>Telefone para contato</h2>
+                </ul>
+
+                <!--O Telefone passa por modificação no css para vira uma imagem-->
+                <a href="">
+                    <h2>Telefone para contato</h2>
                 </a>
 
-        <!--Menu hamburguer inacabado-->
-      </div>
-     <!-- <a href=""><img src="img/menu_hambuguer.svg" alt="" /></a> -->
+                <!--Menu hamburguer inacabado-->
+            </div>
+            <!-- <a href=""><img src="img/menu_hambuguer.svg" alt="" /></a> -->
+        </div>
     </div>
-  </div>
 </div>
