113604cf0415e06dafe5423806313719b2a35217
John-Nero <lennon.pbarros@gmail.com>
2024-06-13 10:34:50 -0300

    icone da pagina alterado

--- sobreNos.php
+++ sobreNos.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sobre nós - Patas e pelos Pethouse</title>
-
+    <link rel="icon" href="img\favicon.ico">
     <!--Reseta o estilo-->
     <link rel="stylesheet" href="css/reset.css" />
 
