c7db0464fe02d4184eb8e3ca50c8d85973939b7f
John-Nero <lennon.pbarros@gmail.com>
2024-06-25 11:49:17 -0300

    começamos o dashboard

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DashBoard Patas e pelos</title>
+    <link rel="icon" href="img\favicon.ico">
+
+    <!--Reseta o estilo-->
+    <link rel="stylesheet" href="css/reset.css" />
+
+    <!--API do google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:wght@900&1display=swap" rel="stylesheet" />
+
+
+    <!--Aponta para o arquivo de estilo-->
+    <link rel="stylesheet" href="css\estilo.css" />
+</head>
+
+<body>
+    <main>
+        <div class="site">
+            <div class="menu_lateral">
+                <img src="..\img\Logo_Patas_e_pelos.svg" alt="">
+                <nav>
+                    <ul>
+                        <li><button><label for=""></label></button></li>
+                        <li><button><label for=""></label></button></li>
+                        <li><button><label for=""></label></button></li>
+                        <li><button><label for=""></label></button></li>
+                        <li><button><label for=""></label></button></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <div class="faixa_superior"></div>
+                <div class="conteudo_central"></div>
+            </div>
+        </div>
+    </main>
+    <!--jQuery-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+</body>
+
+</html>
