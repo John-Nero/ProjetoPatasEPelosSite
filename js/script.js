6c6e19607e859961efac031db9675f0ebde1a511
John-Nero <lennon.pbarros@gmail.com>
2024-07-17 11:01:12 -0300

    adaptação

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+document.getElementById("abrir_menu").addEventListener("click", function () {
+    let menu_lateral = document.getElementById("menu_lateral");
+  
+    menu_lateral.style.height = "100%";
+    menu_lateral.style.width = "250px";
+  });
+  
+  document.getElementById("fechar_menu").addEventListener("click", function () {
+    let menu_lateral = document.getElementById("menu_lateral");
+    menu_lateral.style.height = "0";
+    menu_lateral.style.width = "0";
+  });
+  
+
 document.addEventListener("DOMContentLoaded", function() {
     // Seleciona todos os botões do menu principal
     var menuButtons = document.querySelectorAll('.menu-button');
