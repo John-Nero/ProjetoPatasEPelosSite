9bf69a496e7851b04fa4b2cb7a124124f3e3b5d0
John-Nero <lennon.pbarros@gmail.com>
2024-07-22 08:35:58 -0300

    correção script js para servidor

--- admin/js/script.js
+++ admin/js/script.js
@@ -2,23 +2,29 @@ function filtrar(pagina) {
   switch (pagina) {
     case "banner":
       var status = document.getElementById("filtragemStatusBanner").value;
-      var pagina = document.getElementById("filtragemPaginaBanner").value;
-      window.location.href = "https://pethouse.smpsistema.com.br/john/Patas_e_pelos/admin/index.php?p=" + pagina + "&status=" + status + "&pagina=" + pagina;
+      var paginaSelecionada = document.getElementById("filtragemPaginaBanner").value;
+      console.log(pagina);
+      window.location.href = "https://pethouse.smpsistema.com.br/john/Patas_e_pelos/admin/index.php?p=" + pagina + "&status=" + status + "&pagina=" + paginaSelecionada;
       // Atualizar o valor do campo hidden para enviar ao PHP
-      break;
+    break;
+
     case "depoimento":
       var status = document.getElementById("filtragemStatusDepo").value;
       window.location.href = "https://pethouse.smpsistema.com.br/john/Patas_e_pelos/admin/index.php?p=" + pagina + "&status=" + status;
       // Atualizar o valor do campo hidden para enviar ao PHP
-      break;
+    break;
 
     case "cliente":
       var status = document.getElementById("filtragemStatusCliente").value;
       window.location.href = "https://pethouse.smpsistema.com.br/john/Patas_e_pelos/admin/index.php?p=" + pagina + "&status=" + status;
       // Atualizar o valor do campo hidden para enviar ao PHP
-      break;
-    default:
-      break;
+    break;
+    
+    case "categoria":
+      var status = document.getElementById("filtragemStatusCategoria").value;
+      window.location.href = "https://pethouse.smpsistema.com.br/john/Patas_e_pelos/admin/index.php?p=" + pagina + "&status=" + status;
+      // Atualizar o valor do campo hidden para enviar ao PHP
+    break;
   }
 
   // Submeter o formulário para atualizar a variável PHP
